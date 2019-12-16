b9aaa300d05dabda7b7b0e2ed9e44441585ee0b7
Oksana Kril <oksanakril@Oksanas-iMac.local>
2019-12-15 18:55:36 -0600

    post new project - done

--- routers/projectRouter.js
+++ routers/projectRouter.js
@@ -1,17 +1,30 @@
 const express = require("express");
 const projects = require("../data/helpers/projectModel");
+const { validateProject } = require("../middleware/validate");
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
     projects
-      .get()
-      .then(project => {
-          res.status(200).json(project)
-      })
-      .catch((error) => {
-        next(error)
-      })
+        .get()
+        .then(project => {
+            res.status(200).json(project)
+        })
+        .catch((error) => {
+            next(error)
+        })
   })
+
+router.post('/', validateProject(),  (req, res) => {
+    console.log(req.body)
+    projects
+        .insert(req.body)
+        .then(project => {
+            res.status(201).json(project)
+        })
+        .catch((error) => {
+            next(error)
+        })
+});
 
 module.exports = router;
